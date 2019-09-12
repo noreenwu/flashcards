601756aa57d3c8c0a51477439a6418f3e470c1c8
Noreen Wu <noreenwu@gmail.com>
2019-09-11 22:16:53 -0400

    style: removed styling as not using it yet

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
+import AllDecks from './components/AllDecks'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import AllDecks from './components/AllDecks'
 import NewDeck from './components/NewDeck'
 import DeckDetail from './components/DeckDetail'
 import NewCard from './components/NewCard'
 import Quiz from './components/Quiz'
 import DeckListItem from './components/DeckListItem'
 
+
 class HomeScreen extends React.Component {
   render() {
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="All Decks"
-          onPress={() => this.props.navigation.navigate('AllDecks')}
-        />
-      </View>
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Home Screen: Welcome</Text>
+          <Button
+            title="All Decks"
+            onPress={() => this.props.navigation.navigate('AllDecks')}
+          />
+        </View>
     );
   }
 }
+
+
 
 export const AppNavigator = createStackNavigator(
   {
@@ -40,22 +45,3 @@ export const AppNavigator = createStackNavigator(
 
 
 export default createAppContainer(AppNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
