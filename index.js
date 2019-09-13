842de5e820d04530cab1169569fc5cd8475e74d7
Noreen Wu <noreenwu@gmail.com>
2019-09-12 22:44:55 -0400

    fix: reverting back to no Root component

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import Root from './Root';
+import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Root);
+AppRegistry.registerComponent(appName, () => App);
